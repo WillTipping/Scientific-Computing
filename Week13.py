4427da5816d281bc0715bc24b880c25d1e1f93e7
WillTipping <73531593+WillTipping@users.noreply.github.com>
2021-02-04 14:27:39 +0000

    added week13 file

---
+++ Week13.py
@@ -0,0 +1,14 @@
+#gitbash
+def f(data):
+    list1 = []
+    for i in range(0,len(data)):
+        list1.append(data[i]**2)
+    return list1
+
+data = [2,3,1,-1]
+
+x = f(data)
+
+print(x)
+
+print(x)
